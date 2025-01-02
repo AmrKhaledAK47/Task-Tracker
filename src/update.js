42feae0e017b1653af0eb9c3451ca77dd35ffac7
AmrKhaledAK47 <2002amrkhaled@gmail.com>
2025-01-02 04:36:40 +0200

    Implement task update functionality and modify task description in database

--- src/update.js
+++ src/update.js
@@ -1,19 +1,28 @@
 import { colors } from "./utils.js";
-import { insertDB } from "./db.js";
+import { getDB, saveDB } from "./db.js";
 
-export const add = async (task) => {
-    if (!task) {
-        console.log(`${colors.red}Please provide a task!${colors.reset}`);
+export const update = async (id, updatedTask) => {
+    if (!id || !updatedTask) {
+        console.log(`${colors.red}Please provide an id and a updated task!${colors.reset}`);
         return;
     }
-    const data = {
-        id: Date.now(),
-        description: task,
-        status: 'todo',
-        createdAt: new Date().toISOString(),
-        updatesAt: null
-    };
-
-    await insertDB(data);
-    console.log(`${colors.green}Task added: ${task}${colors.reset}`);
+    if (+id < 0 || isNaN(id)) {
+        console.log(`${colors.red}Please provide a valid id!${colors.reset}`);
+        return;
+    }
+    const { tasks } = await getDB();
+    const match = tasks.filter((task) => task.id === +id);
+    if (match.length === 0) {
+        console.log(`${colors.red}Task not found!${colors.reset}`);
+        return;
+    }
+    const updatedTasks = tasks.map((task) => {
+        if (task.id === +id) {
+            task.description = updatedTask;
+            task.updatesAt = new Date().toISOString();
+        }
+        return task;
+    });
+    await saveDB({ tasks: updatedTasks });
+    console.log(`${colors.green}Task updated: ${updatedTask}${colors.reset}`);
 }
